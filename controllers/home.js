87513ba715592a6ed6c5a12a0d57d3f9569c93ac
Zack Koh <zackkohjunyang@gmail.com>
2014-05-19 00:46:36 +0800

    Modified the Leader Board by adding a Database Query to the homeController, and modifiying the jade file to display some details

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,6 @@
+
+var User = require('../models/User');
+
 /**
  * GET /
  * Home page.
@@ -9,8 +12,25 @@ exports.index = function(req, res) {
   });
 };
 
-exports.getLeaderBoard = function(req, res) {
-  res.render('leaderboard', {
-    title: 'Leader Board'
+exports.getLeaderBoard = function(req, res) {  
+  User.find(function(err, users) {
+    
+    //Create a JSON object that only has what we want to display to prevent security leaks.
+    var usersList = [];
+    
+    for(var i in users) {
+        var item = users[i];
+        usersList.push({
+            "email": item.email,
+            "records": item.records
+        });
+    }
+    
+    console.log(usersList);
+    
+    res.render('leaderboard', {
+      title: 'Leader Board',
+      usersList: users
+    });
   });
 };
